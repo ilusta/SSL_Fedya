37ab141545533e7b1b749ce6983c6c66b092a30f
Arseniy <yarmolinskiyam@gmail.com>
2024-03-17 18:35:11 +0000

    tau lib update

--- lib/tau/Tau.h
+++ lib/tau/Tau.h
@@ -11,7 +11,7 @@ class TauBase{
 public:
     TauBase(float Ts);
     virtual void reset();
-    virtual float process(float in) = 0;
+    virtual float tick(float in) = 0;
     float get_val();
 
 protected:
@@ -28,21 +28,21 @@ public:
     Integrator(float Ts);
     void set(float in);
     void reset() override;
-    float process(float in) override;
+    float tick(float in) override;
 
 protected:
     float I;
 };
 
 /**
- * @brief Реальное дифференцирующее звено первого порядка
+ * @brief Реальное дифференцирующее звено (фильтр высоких частот) первого порядка
  * 
  */
-class FOD : public TauBase {
+class FOHP : public TauBase {
 public:
-    FOD(float Ts, float T, bool is_angle = false);
+    FOHP(float Ts, float T, bool is_angle = false);
     void reset() override;
-    float process(float x) override;
+    float tick(float in) override;
 
 protected:
     Integrator I;
@@ -54,21 +54,58 @@ protected:
  * @brief Фильтр низких частот первого порядка
  * 
  */
-class FOLP : public TauBase {
+class FOD : public TauBase {
 public:
-    FOLP(float Ts, float T);
+    FOD(float Ts, float T);
     void reset() override;
-    float process(float x) override;
+    float tick(float in) override;
 
-private:
+protected:
     Integrator I;
     float T;
+};
+
+/**
+ * @brief Звено насыщения
+ *
+*/
+class Saturation : public TauBase {
+public:
+    Saturation(float min_val, float max_val) : TauBase(0)
+    {
+        this->min_val = min_val;
+        this->max_val = max_val;
+    }
+    void reset() override {}
+    float tick(float in)
+    {
+        return out = (in < min_val) ? min_val : ((in > max_val) ? max_val : in);
+    }
+
+protected:
+    float min_val, max_val;
+};
+
+/**
+ * @brief Звено ограничения первой производной (скорости изменения)
+*/
+class RateLimiter : public TauBase {
+public:
+    RateLimiter(float Ts, float max_der);
+    void reset() override;
+    float tick(float in) override;
+
+protected:
+    Integrator I;
+    Saturation sat;
+    float gain;
+    float max_der;
 };
 
 // class PISD {
 // public:
 //     PISD(float dT, float gain, float kd, float ki, float max_out);
-//     float process(float xerr, float x_i);
+//     float tick(float xerr, float x_i);
 //     float get_val();
 
 // private:
@@ -78,17 +115,4 @@ private:
 //     float _max_out;
 //     Integrator _int;
 //     float _out;
-// };
-
-// class RateLimiter {
-// public:
-//     RateLimiter(float Ts, float max_der);
-//     float process(float x);
-//     float get_val();
-
-// private:
-//     float _out;
-//     Integrator _int;
-//     float _k;
-//     float _max_der;
 // };
